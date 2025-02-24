19e9bfd730af1097d6802f896e71ba7b2835d328
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2025-02-25 00:21:04 +0530

    update

--- playwright.config.js
+++ playwright.config.js
@@ -23,10 +23,6 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'Desktop firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'Pixel 5',
       use: { ...devices['Pixel 5'] },
     },
